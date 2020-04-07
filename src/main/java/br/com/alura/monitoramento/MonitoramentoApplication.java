30c3f296c5af61c39c783619564fdc56565f27b9
Thalles <thalles_jacobs@hotmail.com>
2020-04-07 19:18:09 -0300

    versão inicial

---
+++ src/main/java/br/com/alura/monitoramento/MonitoramentoApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.monitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import br.com.alura.monitoramento.security.config.SecurityConfigurations;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@Import({SecurityConfigurations.class})
+public class MonitoramentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoApplication.class, args);
+	}
+
+}
